c5161207e8a1c267752d2bc4275389de2844e860
Darryl Burke <darryl.burke@gshiftlabs.com>
2015-11-08 19:47:59 -0500

    Update tlc5916_tester.py

--- tlc5916_tester.py
+++ tlc5916_tester.py
@@ -1,8 +1,9 @@
 import tlc5916_driver
-tlc5916_driver.init();
+
 tlc5916_driver.set_pins(11,12,13,15);
 tlc5916_driver.set_debug(False);
 tlc5916_driver.set_chips(2);
 tlc5916_driver.set_offset(0);
+tlc5916_driver.init();
 tlc5916_driver.test();
 tlc5916_driver.clear_all_leds();
