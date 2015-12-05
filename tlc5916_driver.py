39e54d5fc97837de9e3837a6a5c4cf5d420b2e8f
Darryl Burke <darryl.burke@gshiftlabs.com>
2015-12-05 21:51:24 +0000

    board testing

--- tlc5916_driver.py
+++ tlc5916_driver.py
@@ -17,6 +17,7 @@ OEPin=15
 Offset=0
 
 debug=False
+info=False
 pwmFreq=200
 pwmLoad=100
 pwm=0
@@ -56,7 +57,8 @@ def setleds(ledarray):
                     print ("Setting %d : %d -> %d" % (x,y,1))
                  TMPLEDS[x][y]=1
 
-    printarray(TMPLEDS)
+    print ("Array with Set Values")
+    printarray(TMPLEDS,info)
     send_arrays()
     toggleLatch()
 #create an empty array for populating with LEDs to turn on
@@ -65,23 +67,26 @@ def create_empty_array():
     if debug:
        print ("Creating %d Arrays of %d" % (MaxChips,LEDOutputs))
     TMPLEDS=[[0 for x in range(LEDOutputs)] for x in range(MaxChips)]
-    if debug:
-       printarray(TMPLEDS)
+    #if debug:
+    #   printarray(TMPLEDS)
     for x in range(MaxChips):
-        #_arr=[]
         for y in range(LEDOutputs):
             TMPLEDS[x][y]=0
-            #_arr.append(0)
-        #TMPLEDS.append(_arr)
     if debug:
         printarray(TMPLEDS)
-# print the array for diagnostics
-def printarray(myarray):
     if debug:
-        print ("Created Blank Array")
+      print ("Done Creating Empty Array")
+
+# print the array for diagnostics
+def printarray(myarray,force=False):
+    show=debug
+    if force:
+      show=True
+    if show:
+        print ("Printing Array")
     #for i in range(MaxChips):
     #    print("sending %d = %s" % (i, ','.join(str(x) for x in array[i])))
-    if debug:
+    if show:
         print ("Array:")
     for x in range(MaxChips):
         output='  '
@@ -91,8 +96,9 @@ def printarray(myarray):
             val = myarray[x][y]
             output += str(val)
             output += ":"
-        if debug:
+        if show:
             print (output)
+
 #set the pins for Data, Clock, Latch and OE        
 def set_pins(p1,p2,p3,p4):
     global DataOutPin
@@ -111,20 +117,22 @@ def toggleLatch():
 # turn off all leds
 def turn_off():
      set_pwm(0)
-     #GPIO.output(OEPin, True)
 #turn on all leds 100% by default unless specified
 def turn_on(load=100):
     set_pwm(load)
-    #GPIO.output(OEPin, False)
 # light the LEDS according to whats in TMPLED array
-def send_arrays():
+def send_arrays(forceall=False):
    global GPIO
-   for j in range (NumOfChips):
-      rawboard=(NumOfChips-1)
+   forceboards = NumOfChips
+   if forceall:
+     forceboards = MaxChips
+   for j in range (forceboards):
+      rawboard=(forceboards-1)
       rawboard-=j
-      board=(NumOfChips-1)
+      board=(forceboards-1)
       board-=j
-      board+=Offset
+      if Offset==0:
+        board+=Offset
       if debug:
          print ("Offset: %d Old: %d New: %d" % (Offset, rawboard, board))
       for i in range(LEDOutputs):
@@ -140,10 +148,23 @@ def send_arrays():
               GPIO.output(DataOutPin,False)
           GPIO.output(ClockPin,True)
 #clear all LEDs
+def all_leds_on():
+    global TMPLEDS
+    global ledarray
+    print "ALL LEDS ON"
+    create_empty_array()
+    for x in range (MaxChips):
+       for y in range (LEDOutputs):
+             TMPLEDS[x][y]=1
+    printarray(TMPLEDS,info)
+    send_arrays(True)
+    toggleLatch()
+ 
 def clear_leds():
-   for j in range (NumOfChips):
-       setleds([])
-
+    global TMPLEDS
+    create_empty_array()
+    send_arrays(True)
+    toggleLatch()
 def clear_all_leds():
    for j in range (MaxChips):
        setleds([])
@@ -153,6 +174,10 @@ def set_chips(chips):
     global NumOfChips
     NumOfChips = chips
 #set the debug flag on/off
+def set_info( dbg):
+    global info
+    print ("Setting Info to %s" % str(dbg))
+    info=dbg
 def set_debug( dbg):
     global debug
     print ("Setting Debug to %s" % str(dbg))
@@ -167,11 +192,33 @@ def set_offset(value):
     Offset=value;
 # test function
 def test():
+    turn_off()
     print("Running test sequence")
-    turn_on(1)
-    setleds([1,2,11,12,21,22,31,32])
-    #setleds([1,2])
-    #setleds([1])
-    time.sleep(5)
-    setleds([])
+    print("clearing LEDs")
+    print("Chips:%d" %NumOfChips)
+    clear_leds()
+    clear_leds()
+    clear_leds()
+    clear_leds()
+    time.sleep (5);
+    if debug:
+      print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-")
+    for y in range (10):
+      turn_on()
+      all_leds_on()
+      time.sleep(5)
+      clear_leds()
+     
+      for i in range(1,NumOfChips*LEDOutputs+1):
+         print("LED:%s" % i)
+        # setleds([i,i+8,i+16,i+24])
+         setleds([i])
+         time.sleep(1)
+      #for i in range(1,NumOfChips*LEDOutputs+1):
+      #   print("LED:%s" % i)
+      #   setleds([i])
+      #   time.sleep(1)
+      turn_off()
+      clear_leds()
+      time.sleep(5)
     GPIO.cleanup()
--- tlc5916_tester.py
+++ tlc5916_tester.py
@@ -1,8 +1,9 @@
 import tlc5916_driver
 
 tlc5916_driver.set_pins(11,12,13,15);
-tlc5916_driver.set_debug(False);
-tlc5916_driver.set_chips(2);
+#tlc5916_driver.set_debug(True);
+tlc5916_driver.set_info(True);
+tlc5916_driver.set_chips(4);
 tlc5916_driver.set_offset(0);
 tlc5916_driver.init();
 tlc5916_driver.test();
